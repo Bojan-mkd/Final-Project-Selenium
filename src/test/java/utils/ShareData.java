4ca58f4fdf0d52a7c9a3159952a5373db7307f5d
Bojan-mkd <bmihajlovski90@gmail.com>
2024-08-03 16:03:51 +0200

    code format

--- src/test/java/utils/ShareData.java
+++ src/test/java/utils/ShareData.java
@@ -30,6 +30,8 @@ public class ShareData {
     public static String shareShippingMethodsPrice;
     public static String shareShippingMethodsText;
 
+    public static String shareProxyEmail;
+
     public static void setCheckoutPageArg(String email, String firstName, String lastName, String address1, String address2, String address3, String city, String state, String zipCode, String country, String phoneNumber, String shippingMethod) {
         shareEmail = email;
         shareFirstName = firstName;
@@ -94,7 +96,7 @@ public class ShareData {
     }
 
     //Storing products arguments for sharing with different test case
-    public static void setProductPageArg(String sizeNumber1, String sizeNumber2, String color1, String color2, String productNameOne, String productNameTwo){
+    public static void setProductPageArg(String sizeNumber1, String sizeNumber2, String color1, String color2, String productNameOne, String productNameTwo) {
         shareProductNameOne = productNameOne;
         shareProductNameTwo = productNameTwo;
         shareSizeNumber1 = sizeNumber1;
@@ -126,25 +128,27 @@ public class ShareData {
     public static String getShareColor2() {
         return shareColor2;
     }
+
     //Price of the product that is added directly to shopping cart from the catalog
-    public static void setPriceOfProductAddedDirectlyFromCatalog(String priceOfProductAddedDirectlyFromCatalog){
-        sharePriceOfProductAddedDirectlyFromCatalog = priceOfProductAddedDirectlyFromCatalog ;
+    public static void setPriceOfProductAddedDirectlyFromCatalog(String priceOfProductAddedDirectlyFromCatalog) {
+        sharePriceOfProductAddedDirectlyFromCatalog = priceOfProductAddedDirectlyFromCatalog;
     }
 
-    public static String getSharePriceOfProductAddedDirectlyFromCatalog(){
-        return sharePriceOfProductAddedDirectlyFromCatalog.replace("$","");
+    public static String getSharePriceOfProductAddedDirectlyFromCatalog() {
+        return sharePriceOfProductAddedDirectlyFromCatalog.replace("$", "");
     }
 
     //Price of the product that is chosen directly from Catalog and after add to shopping cart
-    public static void setPriceOfProductChosenFromCatalog(String priceOfProductChosenFromCatalog){
+    public static void setPriceOfProductChosenFromCatalog(String priceOfProductChosenFromCatalog) {
         sharePriceOfProductChosenFromCatalog = priceOfProductChosenFromCatalog;
     }
 
-    public static String getSharePriceOfProductChosenFromCatalog(){
-        return sharePriceOfProductChosenFromCatalog.replace("$","");
+    public static String getSharePriceOfProductChosenFromCatalog() {
+        return sharePriceOfProductChosenFromCatalog.replace("$", "");
     }
+
     //Sum of both prices of the products
-    public static String totalSumOfProductPrice(String num1, String num2){
+    public static String totalSumOfProductPrice(String num1, String num2) {
         double n1 = Double.parseDouble(num1);
         double n2 = Double.parseDouble(num2);
         double totalSum = n1 + n2;
@@ -153,21 +157,31 @@ public class ShareData {
         DecimalFormat decimalFormat = new DecimalFormat("#.00", decimalFormatSymbols);
         return decimalFormat.format(totalSum);
     }
+
     //Storing shipping methods price
-    public static void setShippingMethodsPrice(String price){
+    public static void setShippingMethodsPrice(String price) {
         shareShippingMethodsPrice = price;
     }
 
-    public static String getShareShippingMethodsPrice(){
+    public static String getShareShippingMethodsPrice() {
         return shareShippingMethodsPrice;
     }
+
     //Storing shipping methods text
-    public static void setShippingMethodsText(String text){
+    public static void setShippingMethodsText(String text) {
         shareShippingMethodsText = text;
     }
 
-    public static String getShareShippingMethodsText(){
+    public static String getShareShippingMethodsText() {
         return shareShippingMethodsText;
+    }
+
+    public static void setProxyEmailStoring(String proxyEmail) {
+        shareProxyEmail = proxyEmail;
+    }
+
+    public static String getProxyEmailStoring() {
+        return shareProxyEmail;
     }
 
 
