1f92af8c97d080939cb0b73ea4e7996acac5e699
Bojan-mkd <bmihajlovski90@gmail.com>
2024-08-04 18:14:03 +0200

    Correction in string for assertion

--- src/test/java/tests/LumaAddAndCheckoutProductsToShoppingCartTest.java
+++ src/test/java/tests/LumaAddAndCheckoutProductsToShoppingCartTest.java
@@ -19,6 +19,7 @@ public class LumaAddAndCheckoutProductsToShoppingCartTest extends BaseClassBefor
         LumaProductsCatalogPage productsPage = new LumaProductsCatalogPage(driver, wait, action);
         LumaSingleProductPage singleProduct = new LumaSingleProductPage(driver, action);
         LumaCheckoutPage checkoutPage = new LumaCheckoutPage(driver, wait, action);
+        //Getting to products by sidebar option+
         homePage.womenCategoryButton();
         homePage.choosingPantsCategory();
         //Adding first product via left side shopping options and adding it directly to shopping cart
@@ -49,7 +50,59 @@ public class LumaAddAndCheckoutProductsToShoppingCartTest extends BaseClassBefor
         softAssert.assertEquals(subTotalSum.replace("$",""), ShareData.totalSumOfProductPrice(ShareData.getSharePriceOfProductChosenFromCatalog(), ShareData.getSharePriceOfProductAddedDirectlyFromCatalog()));
         productsPage.shoppingCartCheckout();
         //Checking if both of products are present in order summary
-        softAssert.assertEquals(checkoutPage.assertingCheckoutPage(), "Shipping Address");
+        softAssert.assertEquals(checkoutPage.assertingCheckoutPage(), "Shipping");
+        softAssert.assertEquals(checkoutPage.assertingOrderSummary(), "Order Summary");
+        softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameOne).contains(productNameOne));
+        softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameOne).contains(sizeNumber1));
+        softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameOne).contains(color1));
+        softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameOne).contains(ShareData.getSharePriceOfProductAddedDirectlyFromCatalog()));
+        softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameTwo).contains(productNameTwo));
+        softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameTwo).contains(sizeNumber2));
+        softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameTwo).contains(color2));
+        softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameTwo).contains(ShareData.getSharePriceOfProductChosenFromCatalog()));
+        softAssert.assertAll();
+    }
+
+    @Test(dataProvider = "informationForTwoProducts", dataProviderClass = DataProviders.class)
+    public void addAndCheckoutProductInShoppingCartWithHover(String sizeNumber1, String sizeNumber2, String color1, String color2, String productNameOne, String productNameTwo) throws InterruptedException {
+        softAssert = new SoftAssert();
+        LumaHomePage homePage = new LumaHomePage(driver, action);
+        LumaProductsCatalogPage productsPage = new LumaProductsCatalogPage(driver, wait, action);
+        LumaSingleProductPage singleProduct = new LumaSingleProductPage(driver, action);
+        LumaCheckoutPage checkoutPage = new LumaCheckoutPage(driver, wait, action);
+        //Getting to products via hover option
+        homePage.womenCategoryButton2();
+        homePage.womenBottomCategory();
+        homePage.choosingWomensPantsCategory2();
+        //Adding first product via left side shopping options and adding it directly to shopping cart
+        productsPage.sizeCategoryList(sizeNumber1);
+        productsPage.colorCategoryList(color1);
+        productsPage.addingProductToCartDirectlyFromCatalogList(productNameOne);
+        softAssert.assertEquals(productsPage.confirmationMessageForSuccessfulAddingProductToCart(), "You added " + productNameOne + " to your shopping cart.");
+        productsPage.clearAllShoppingOptions();
+        //Adding second product via clicking on the product directly and open single product page to choose and adding to shopping cart
+        productsPage.choosingProductDirectlyFromCatalogList(productNameTwo);
+        singleProduct.scrollToAddToWish();
+        ShareData.setPriceOfProductChosenFromCatalog(singleProduct.priceOfProduct());
+        singleProduct.choosingSizeOrColor(sizeNumber2);
+        singleProduct.choosingSizeOrColor(color2);
+        singleProduct.addToCartButton();
+        softAssert.assertEquals(productsPage.confirmationMessageForSuccessfulAddingProductToCart(), "You added " + productNameTwo + " to your shopping cart.");
+        productsPage.shoppingCartButton();
+        //Shopping cart window pop-up assert of name, color, size and price of the chosen products
+        softAssert.assertTrue(productsPage.productsInShoppingCart(productNameTwo).contains(productNameTwo));
+        softAssert.assertTrue(productsPage.productsInShoppingCart(productNameTwo).contains(sizeNumber2));
+        softAssert.assertTrue(productsPage.productsInShoppingCart(productNameTwo).contains(color2));
+        softAssert.assertTrue(productsPage.productsInShoppingCart(productNameTwo).contains(ShareData.getSharePriceOfProductChosenFromCatalog()));
+        softAssert.assertTrue(productsPage.productsInShoppingCart(productNameOne).contains(productNameOne));
+        softAssert.assertTrue(productsPage.productsInShoppingCart(productNameOne).contains(sizeNumber1));
+        softAssert.assertTrue(productsPage.productsInShoppingCart(productNameOne).contains(color1));
+        softAssert.assertTrue(productsPage.productsInShoppingCart(productNameOne).contains(ShareData.getSharePriceOfProductAddedDirectlyFromCatalog()));
+        String subTotalSum = productsPage.subtotalSumForBothProductsInShoppingCart();
+        softAssert.assertEquals(subTotalSum.replace("$",""), ShareData.totalSumOfProductPrice(ShareData.getSharePriceOfProductChosenFromCatalog(), ShareData.getSharePriceOfProductAddedDirectlyFromCatalog()));
+        productsPage.shoppingCartCheckout();
+        //Checking if both of products are present in order summary
+        softAssert.assertEquals(checkoutPage.assertingCheckoutPage(), "Shipping");
         softAssert.assertEquals(checkoutPage.assertingOrderSummary(), "Order Summary");
         softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameOne).contains(productNameOne));
         softAssert.assertTrue(checkoutPage.productsInOrderSummary(productNameOne).contains(sizeNumber1));
