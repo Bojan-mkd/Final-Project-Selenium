0f7e95a037d93c874fc6c8b23d5179e84e890e5b
Bojan-mkd <bmihajlovski90@gmail.com>
2024-08-04 18:07:47 +0200

    Is added action to log out account

--- src/test/java/pages/LumaAccountPage.java
+++ src/test/java/pages/LumaAccountPage.java
@@ -10,6 +10,8 @@ public class LumaAccountPage {
     WebDriverWait wait;
     private By confirmationMessageForSuccessfulRegistrationLocator = By.xpath("//div[@data-ui-id='message-success']/div");
     private By accountPageTitleLocator = By.xpath("//h1[@class='page-title']");
+    private By welcomeUserDropDownLocator = By.xpath("//div[@class='panel header']/ul/li[2]/span/button");
+    private By signingOutUserButtonLocator = By.xpath("//div[@class='panel header']/ul/li[2]/div/ul/li[3]");
 
     public LumaAccountPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
@@ -22,5 +24,10 @@ public class LumaAccountPage {
 
     public String accountPageTitle() {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(accountPageTitleLocator)).getText();
+    }
+
+    public void signingOutUserButton(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(welcomeUserDropDownLocator)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(signingOutUserButtonLocator)).click();
     }
 }
